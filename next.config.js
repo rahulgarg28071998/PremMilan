6ca61839f8ee380f563fb805821b759a3cbd8096
rahulgarg28071998 <rgarg8438@gmail.com>
2024-06-01 03:16:02 +0530

    test

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   trailingSlash: true,
   basePath: '',
+  distDir: 'out',
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
